ca43fd585991fe2c63030210f526f852b575744e
Biopreventai <39650800+Biopreventai@users.noreply.github.com>
2018-06-28 09:36:30 +0200

    Add files via upload

---
+++ page4.js
@@ -0,0 +1,8 @@
+$(document).ready(function()
+{
+   function skrollrInit()
+   {
+      skrollr.init({forceHeight: false, mobileCheck: function() { return false; }, smoothScrolling: false});
+   }
+   skrollrInit();
+});
